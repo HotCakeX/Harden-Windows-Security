ca3815a42e292ab16137f9e37c45e0dc998820e4
HotCakeX <118815227+HotCakeX@users.noreply.github.com>
2023-02-08 09:09:12 +0000

    Update Hashrenewal.ps1
    Signed-off-by: HotCakeX <118815227+HotCakeX@users.noreply.github.com>

--- Workflowstuff/Hashrenewal.ps1
+++ Workflowstuff/Hashrenewal.ps1
@@ -1,86 +1,43 @@
-$wc = [System.Net.WebClient]::new()
-$pkgurl = 'https://github.com/HotCakeX/Harden-Windows-Security/raw/main/GroupPolicy/Security-Baselines-X.zip'
-$FileHashSHA256 = Get-FileHash -Algorithm SHA256 -InputStream ($wc.OpenRead($pkgurl))
-$FileHashSHA512 = Get-FileHash -Algorithm SHA512 -InputStream ($wc.OpenRead($pkgurl))
+if ( Get-Module -ListAvailable "VirusTotalAnalyzer") {
+    Import-Module VirusTotalAnalyzer -Force
+} else {
+    Install-Module -Name VirusTotalAnalyzer -AllowClobber -Force -Scope CurrentUser
+    Import-Module VirusTotalAnalyzer -Force
+}
+$VTApi = ${{ secrets.VTAPI }}
 
-$FileHashSHA256 = $FileHashSHA256.Hash
-$FileHashSHA512 = $FileHashSHA512.Hash
+# Submit
+$Output = New-VirusScan -ApiKey $VTApi -File "FILE_HERE"
+$Output | Format-List
 
+# Wait
+Do {
+    $OutputScan = Get-VirusReport -ApiKey $VTApi -AnalysisId $Output.data.id
+    if ($OutputScan.data.attributes.status -eq 'queued') {
+        Write-Host "Waiting..." -ForegroundColor Gray
+        Start-Sleep 10
+    }
+}
+until($OutputScan.data.attributes.status -eq 'completed')
 
-
-$SHA256 = @"
-
-``````
-$FileHashSHA256
-``````
-
-"@
-
-$readme = Get-Content -raw -path "README.md"
-$readme = $readme -replace "(?s)(?<=<!-- SHA-256-Hash:START -->).*(?=<!-- SHA-256-Hash:END -->)", $SHA256
- set-Content -path "README.md" -Value $readme.TrimEnd()
-
-
-
-
-
-
-$SHA512 = @"
-
-``````
-$FileHashSHA512
-``````
-
-"@
-
-$readme = Get-Content -raw -path "README.md"
-$readme = $readme -replace "(?s)(?<=<!-- SHA-512-Hash:START -->).*(?=<!-- SHA-512-Hash:END -->)", $SHA512
- set-Content -path "README.md" -Value $readme.TrimEnd()
- 
-
-
-
-
- 
-
-
-
-
-<#//////////////////////#>
-
-
-
-$SHA256 = @"
-
-``````
-$FileHashSHA256
-``````
-
-"@
-
-
-
-
-
- 
-$HashVerification = @"
-
-``````PowerShell
-`$WebClient = [System.Net.WebClient]::new()
-`$PackageURL = 'https://github.com/HotCakeX/Harden-Windows-Security/raw/main/GroupPolicy/Security-Baselines-X.zip'
-`$publishedHashSHA256 = `'$FileHashSHA256`'
-`$publishedHashSHA512 = `'$FileHashSHA512`'
-`$SHA256Hash = Get-FileHash -Algorithm SHA256 -InputStream (`$WebClient.OpenRead(`$PackageURL))
-`$SHA512Hash = Get-FileHash -Algorithm SHA512 -InputStream (`$WebClient.OpenRead(`$PackageURL))
-`$SHA256Hash.Hash -eq `$publishedHashSHA256 -and `$SHA512Hash.Hash -eq `$publishedHashSHA512
-``````
-
-"@
-
-
-
-
-
-$readme = Get-Content -raw -path "README.md"
-$readme = $readme -replace "(?s)(?<=<!-- Hash-Verification:START -->).*(?=<!-- Hash-Verification:END -->)", $HashVerification
- set-Content -path "README.md" -Value $readme.TrimEnd()
+# Result
+Write-Host "Analyze completed" -ForegroundColor DarkMagenta
+if ($OutputScan.data.attributes.stats.suspicious -gt 0 -or $OutputScan.data.attributes.stats.malicious -gt 0) {
+    Write-Host ("sha256: {0}`nUndetected: {1}`nSuspicious: {2}`nMalicious: {3}`nURL: {4}" -f `
+            $OutputScan.meta.file_info.sha256, `
+            $OutputScan.data.attributes.stats.undetected, `
+            $OutputScan.data.attributes.stats.suspicious, `
+            $OutputScan.data.attributes.stats.malicious,
+        "https://www.virustotal.com/gui/file/$($OutputScan.meta.file_info.sha256)"
+    ) -ForegroundColor Red
+    Exit 1
+} else {
+    Write-Host ("sha256: {0}`nUndetected: {1}`nSuspicious: {2}`nMalicious: {3}`nURL: {4}" -f `
+            $OutputScan.meta.file_info.sha256, `
+            $OutputScan.data.attributes.stats.undetected, `
+            $OutputScan.data.attributes.stats.suspicious, `
+            $OutputScan.data.attributes.stats.malicious,
+        "https://www.virustotal.com/gui/file/$($OutputScan.meta.file_info.sha256)"
+    ) -ForegroundColor Green
+    Exit 0
+}

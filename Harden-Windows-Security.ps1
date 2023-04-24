ced8d946f6e199b68e38915e7e93416d9c8289f7
HotCakeX <spynetgirl@outlook.com>
2023-04-24 15:25:41 +0330

    self-update test 2023.4.24.2

--- Harden-Windows-Security.ps1
+++ Harden-Windows-Security.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 2023.4.24.1
+.VERSION 2023.4.24.2
 
 .GUID d435a293-c9ee-4217-8dc1-4ad2318a5770
 
@@ -233,7 +233,7 @@ try {
     }
 
     # Check the current hard-coded version against the latest version online and self-update if necessary
-    $currentVersion = '2023.4.24.1'
+    $currentVersion = '2023.4.24.2'
     try {
         $latestVersion = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/HotCakeX/Harden-Windows-Security/main/Version.txt"
     }

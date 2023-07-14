5a8bac66bf423a2e3b63adbcdfde49fa8649de8c
Violet <spynetgirl@outlook.com>
2023-07-14 23:27:42 +0100

    Update Resources2.ps1
    Updated Resources2 for WDAC simulation to improve the performance and logic for double signed files

--- WDACConfig/Resources2.ps1
+++ WDACConfig/Resources2.ps1
@@ -85,13 +85,16 @@ function Get-TBSCertificate {
 
 
 # Helps get the 2nd aka nested signer/signature of the dual signed files
+# https://www.sysadmins.lv/blog-en/reading-multiple-signatures-from-signed-file-with-powershell.aspx
+# https://www.sysadmins.lv/disclaimer.aspx
 function Get-AuthenticodeSignatureEx {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [String[]]$FilePath
+        [String[]]$FilePath # The path of the file(s) to get the signature of
     )
     begin {
+        # Define the signature of the Crypt32.dll library functions to use
         $signature = @"
     [DllImport("crypt32.dll", CharSet = CharSet.Auto, SetLastError = true)]
     public static extern bool CryptQueryObject(
@@ -126,36 +129,49 @@ function Get-AuthenticodeSignatureEx {
         int dwFlags
     );
 "@
+        # Load the System.Security assembly to use the SignedCms class
         Add-Type -AssemblyName System.Security
+        # Add the Crypt32.dll library functions as a type
         Add-Type -MemberDefinition $signature -Namespace PKI -Name Crypt32
+        # Define some constants for the CryptQueryObject function parameters
         $CERT_QUERY_OBJECT_FILE = 0x1
         $CERT_QUERY_CONTENT_FLAG_PKCS7_SIGNED_EMBED = 0x400
         $CERT_QUERY_FORMAT_FLAG_BINARY = 0x2
             
+        # Define a helper function to get the timestamps of the countersigners
         function getTimeStamps($SignerInfo) {
             $retValue = @()
             foreach ($CounterSignerInfos in $Infos.CounterSignerInfos) {                    
+                # Get the signing time attribute from the countersigner info object
                 $sTime = ($CounterSignerInfos.SignedAttributes | Where-Object { $_.Oid.Value -eq "1.2.840.113549.1.9.5" }).Values | `
                     Where-Object { $null -ne $_.SigningTime }
+                # Create a custom object with the countersigner certificate and signing time properties
                 $tsObject = New-Object psobject -Property @{
                     Certificate = $CounterSignerInfos.Certificate
                     SigningTime = $sTime.SigningTime.ToLocalTime()
                 }
+                # Add the custom object to the return value array
                 $retValue += $tsObject
             }
+            # Return the array of custom objects with countersigner info
             $retValue
+
         }
     }
     process {
+        # For each file path, get the authenticode signature using the built-in cmdlet
         Get-AuthenticodeSignature $FilePath | ForEach-Object {
-            $Output = $_
-            if ($null -ne $Output.SignerCertificate) {              
+            $Output = $_ # Store the output object in a variable
+            if ($null -ne $Output.SignerCertificate) {
+                # If the output object has a signer certificate property
+                # Initialize some variables to store the output parameters of the CryptQueryObject function
                 $pdwMsgAndCertEncodingType = 0
                 $pdwContentType = 0
                 $pdwFormatType = 0
                 [IntPtr]$phCertStore = [IntPtr]::Zero
                 [IntPtr]$phMsg = [IntPtr]::Zero
                 [IntPtr]$ppvContext = [IntPtr]::Zero
+                # Call the CryptQueryObject function to get the handle of the PKCS #7 message from the file path
                 $return = [PKI.Crypt32]::CryptQueryObject(
                     $CERT_QUERY_OBJECT_FILE,
                     $Output.Path,
@@ -169,37 +185,50 @@ function Get-AuthenticodeSignatureEx {
                     [ref]$phMsg,
                     [ref]$ppvContext
                 )
-                if (!$return) { return }
-                $pcbData = 0
+                if (!$return) { return } # If the function fails, return nothing
+                $pcbData = 0 # Initialize a variable to store the size of the PKCS #7 message data
+                # Call the CryptMsgGetParam function to get the size of the PKCS #7 message data
                 $return = [PKI.Crypt32]::CryptMsgGetParam($phMsg, 29, 0, $null, [ref]$pcbData)
-                if (!$return) { return }
-                $pvData = New-Object byte[] -ArgumentList $pcbData
+                if (!$return) { return } # If the function fails, return nothing
+                $pvData = New-Object byte[] -ArgumentList $pcbData # Create a byte array to store the PKCS #7 message data
+                # Call the CryptMsgGetParam function again to get the PKCS #7 message data
                 $return = [PKI.Crypt32]::CryptMsgGetParam($phMsg, 29, 0, $pvData, [ref]$pcbData)
-                $SignedCms = New-Object Security.Cryptography.Pkcs.SignedCms
-                $SignedCms.Decode($pvData)
-                $Infos = $SignedCms.SignerInfos[0]
+                $SignedCms = New-Object Security.Cryptography.Pkcs.SignedCms # Create a SignedCms object to decode the PKCS #7 message data
+                $SignedCms.Decode($pvData) # Decode the PKCS #7 message data and populate the SignedCms object properties
+                $Infos = $SignedCms.SignerInfos[0] # Get the first signer info object from the SignedCms object
+                # Add some properties to the output object, such as TimeStamps, DigestAlgorithm and NestedSignature
                 $Output | Add-Member -MemberType NoteProperty -Name TimeStamps -Value $null
                 $Output | Add-Member -MemberType NoteProperty -Name DigestAlgorithm -Value $Infos.DigestAlgorithm.FriendlyName
-                $Output.TimeStamps = getTimeStamps $Infos
+                # Call the helper function to get the timestamps of the countersigners and assign it to the TimeStamps property
+                $Output.TimeStamps = getTimeStamps $Infos 
+                # Check if there is a nested signature attribute in the signer info object by looking for the OID 1.3.6.1.4.1.311.2.4.1
                 $second = $Infos.UnsignedAttributes | Where-Object { $_.Oid.Value -eq "1.3.6.1.4.1.311.2.4.1" }
                 if ($second) {
+                    # If there is a nested signature attribute
+                    # Get the value of the nested signature attribute as a raw data byte array
                     $value = $second.Values | Where-Object { $_.Oid.Value -eq "1.3.6.1.4.1.311.2.4.1" }
-                    $SignedCms2 = New-Object Security.Cryptography.Pkcs.SignedCms
-                    $SignedCms2.Decode($value.RawData)
-                    $Output | Add-Member -MemberType NoteProperty -Name NestedSignature -Value $null
-                    $Infos = $SignedCms2.SignerInfos[0]
+                    $SignedCms2 = New-Object Security.Cryptography.Pkcs.SignedCms # Create another SignedCms object to decode the nested signature data
+                    $SignedCms2.Decode($value.RawData) # Decode the nested signature data and populate the SignedCms object properties
+                    $Output | Add-Member -MemberType NoteProperty -Name NestedSignature -Value $null 
+                    $Infos = $SignedCms2.SignerInfos[0] # Get the first signer info object from the nested signature SignedCms object
+                    # Create a custom object with some properties of the nested signature, such as signer certificate, digest algorithm and timestamps
                     $nested = New-Object psobject -Property @{
                         SignerCertificate = $Infos.Certificate
                         DigestAlgorithm   = $Infos.DigestAlgorithm.FriendlyName
                         TimeStamps        = getTimeStamps $Infos
                     }
+                    # Assign the custom object to the NestedSignature property of the output object
                     $Output.NestedSignature = $nested
                 }
+                # Return the output object with the added properties
                 $Output
+                # Close the handles of the PKCS #7 message and the certificate store
                 [void][PKI.Crypt32]::CryptMsgClose($phMsg)
                 [void][PKI.Crypt32]::CertCloseStore($phCertStore, 0)
             }
             else {
+                # If the output object does not have a signer certificate property
+                # Return the output object as it is
                 $Output
             }
         }
@@ -301,8 +330,6 @@ function Get-CertificateDetails {
         Write-Output "Please specify one of the following switch parameters to get certificate details: -IntermediateOnly, -AllCertificates, or -LeafCertificate."
     }
 }
-  
-
 
 
 
@@ -316,19 +343,27 @@ function Compare-SignerAndCertificate {
         [Parameter(Mandatory = $true)] [string]$SignedFilePath
     )
   
+
     # Get the signer information from the XML file path using the Get-SignerInfo function
-    $SignerInfo = Get-SignerInfo -XmlFilePath $XmlFilePath
-  
-    
+    $SignerInfo = Get-SignerInfo -XmlFilePath $XmlFilePath  
+   
     # Declare $CertificateDetails as an array
     $CertificateDetails = @()
+
+    # Declare $NestedCertificateDetails as an array 
+    $NestedCertificateDetails = @()
 
     # Get the certificate details from the signed file path using the Get-CertificateDetails function with the IntermediateOnly switch parameter
     $CertificateDetails += Get-CertificateDetails -IntermediateOnly -FilePath $SignedFilePath
 
-    if ($null -ne (Get-AuthenticodeSignatureEx -FilePath $SignedFilePath).NestedSignature.SignerCertificate) {
+    $ExtraCertificateDetails = Get-AuthenticodeSignatureEx -FilePath $SignedFilePath
+
+    $NestedCertificate = ($ExtraCertificateDetails).NestedSignature.SignerCertificate
+
+    # Store Intermediate Certificate details of the 2nd certificate into another array variable if it exists
+    if ($null -ne $NestedCertificate) {
         # append an X509Certificate2 object to the array
-        $CertificateDetails += Get-CertificateDetails -IntermediateOnly -X509Certificate2 $((Get-AuthenticodeSignatureEx -FilePath $SignedFilePath).NestedSignature.SignerCertificate)
+        $NestedCertificateDetails += Get-CertificateDetails -IntermediateOnly -X509Certificate2 $NestedCertificate
     }
 
 
@@ -354,7 +389,7 @@ function Compare-SignerAndCertificate {
             FilePath            = $SignedFilePath # Add the file path to the object
         }
   
-        # Loop through each certificate in the certificate details array
+        # Loop through each certificate in the certificate details array of the Main Cert
         foreach ($Certificate in $CertificateDetails) {
             # Check if the signer's CertRoot (referring to the TBS value in the xml file which belongs to an intermediate cert of the file)...
             # ...matches the TBSValue of the file's certificate (TBS values of one of the intermediate certificates of the file since -IntermediateOnly parameter is used earlier and that's what FilePublisher level uses)
@@ -365,9 +400,15 @@ function Compare-SignerAndCertificate {
                 $ComparisonResult.CertIssuerCN = $Certificate.IssuerCN
                 $ComparisonResult.CertNotAfter = $Certificate.NotAfter
                 $ComparisonResult.CertTBSValue = $Certificate.TBSValue
-                $ComparisonResult.CertRootMatch = $true # meaning one of the TBS values of the file's intermediate certs is in the xml file signers's TBS values
-  
-                # Check if the signer's Name matches the certificate's SubjectCN
+                # if file has nested signature, only set a flag instead of setting the entire property to true
+                if ($null -ne $NestedCertificate) {
+                    $CertRootMatchPart1 = $true
+                }
+                else {
+                    $ComparisonResult.CertRootMatch = $true # meaning one of the TBS values of the file's intermediate certs is in the xml file signers's TBS values
+                }
+
+                # Check if the signer's Name matches the Intermediate certificate's SubjectCN
                 if ($Signer.Name -eq $Certificate.SubjectCN) {
                     # If yes, set the CertNameMatch to true
                     $ComparisonResult.CertNameMatch = $true # this should naturally be always true like the CertRootMatch because this is the CN of the same cert that has its TBS value in the xml file in signers
@@ -377,7 +418,62 @@ function Compare-SignerAndCertificate {
                 break
             }
         }
+
+
+
+
+
+
+        # Nested Certificate TBS processing
+        if ($null -ne $NestedCertificate) {
+
+            # Loop through each certificate in the NESTED certificate details array
+            foreach ($Certificate in $NestedCertificateDetails) {
+                # Check if the signer's CertRoot (referring to the TBS value in the xml file which belongs to an intermediate cert of the file)...
+                # ...matches the TBSValue of the file's certificate (TBS values of one of the intermediate certificates of the file since -IntermediateOnly parameter is used earlier and that's what FilePublisher level uses)
+                # So this checks to see if the Signer's TBS value in xml matches any of the TBS value(s) of the file's intermediate certificate(s), if yes, that means that file is allowed to run by WDAC engine
+                if ($Signer.CertRoot -eq $Certificate.TBSValue) {
+                    # If yes, assign the certificate properties to the comparison result object and set the CertRootMatch to true
+                    $ComparisonResult.CertSubjectCN = $Certificate.SubjectCN
+                    $ComparisonResult.CertIssuerCN = $Certificate.IssuerCN
+                    $ComparisonResult.CertNotAfter = $Certificate.NotAfter
+                    $ComparisonResult.CertTBSValue = $Certificate.TBSValue       
+                    # When file has nested signature, only set a flag instead of setting the entire property to true             
+                    $CertRootMatchPart2 = $true
+
+                    # Check if the signer's Name matches the Intermediate certificate's SubjectCN
+                    if ($Signer.Name -eq $Certificate.SubjectCN) {
+                        # If yes, set the CertNameMatch to true
+                        $ComparisonResult.CertNameMatch = $true # this should naturally be always true like the CertRootMatch because this is the CN of the same cert that has its TBS value in the xml file in signers
+                    }
   
+                    # Break out of the inner loop since we found a match for this signer
+                    break
+                }
+            }
+        }
+
+
+
+        # if file has nested certificates
+        if ($null -ne $NestedCertificate) {
+            # check if both of the file's nested vertificates are available in the Signers in xml policy
+            if ( $CertRootMatchPart1 -eq $true -and $CertRootMatchPart2 -eq $true) {
+                $ComparisonResult.CertRootMatch = $true # meaning all of the TBS values of the double signed file's intermediate certificates exists in the xml file's signers' TBS values
+                                                        
+            }
+            else {
+                $ComparisonResult.CertRootMatch = $false 
+            }
+        }
+        
+
+ 
+
+
+
+
+
         # Add the comparison result object to the comparison results array
         $ComparisonResults += $ComparisonResult
   
@@ -385,15 +481,25 @@ function Compare-SignerAndCertificate {
 
     # Declare $LeafCertificateDetails as an array
     $LeafCertificateDetails = @()
+
+    # Declare $NestedLeafCertificateDetails as an array
+    $NestedLeafCertificateDetails = @()
   
     # Get the leaf certificate details from the signed file path
     $LeafCertificateDetails += Get-CertificateDetails -LeafCertificate -FilePath $SignedFilePath
 
 
-    if ($null -ne (Get-AuthenticodeSignatureEx -FilePath $SignedFilePath).NestedSignature.SignerCertificate) {
-        # Use += to append an X509Certificate2 object to the array
-        $CertificateDetails += Get-CertificateDetails -LeafCertificate -X509Certificate2 $((Get-AuthenticodeSignatureEx -FilePath $SignedFilePath).NestedSignature.SignerCertificate)
+    # Store Leaf Certificate details of the 2nd certificate into another array variable if it exists
+    if ($null -ne $NestedCertificate) {
+        # append an X509Certificate2 object to the array
+        $NestedLeafCertificateDetails += Get-CertificateDetails -LeafCertificate -X509Certificate2 $NestedCertificate
     }
+
+
+
+
+
+
 
   
     # Loop through each signer in the signer information array again

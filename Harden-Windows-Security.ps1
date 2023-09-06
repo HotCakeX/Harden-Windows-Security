000f7df98e5b26b15ba7109583c06506caea7f93
HotCakeX <spynetgirl@outlook.com>
2023-09-06 02:21:06 -1000

    Improved Process mitigations CSV
    Removed extra words from the CSV
    Changed the CSV mitigations file link temporarily to the test branch

--- Harden-Windows-Security.ps1
+++ Harden-Windows-Security.ps1
@@ -422,7 +422,7 @@ try {
 
                 # Download Process Mitigations CSV file from GitHub or Azure DevOps
                 try {
-                    Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/HotCakeX/Harden-Windows-Security/main/Payload/ProcessMitigations.csv' -OutFile '.\ProcessMitigations.csv' -ErrorAction Stop
+                    Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/HotCakeX/Harden-Windows-Security/Harden-Windows-Security-Update/Payload/ProcessMitigations.csv' -OutFile '.\ProcessMitigations.csv' -ErrorAction Stop
                 }
                 catch {
                     Write-Host 'Using Azure DevOps...' -ForegroundColor Yellow

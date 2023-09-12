ea5d8a8e21433804076ff3c1669612d2643ef3a2
HotCakeX <spynetgirl@outlook.com>
2023-09-12 13:44:46 +0100

    Update Harden-Windows-Security.ps1

--- Harden-Windows-Security.ps1
+++ Harden-Windows-Security.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 2023.9.06
+.VERSION 2023.9.12
 
 .GUID d435a293-c9ee-4217-8dc1-4ad2318a5770
 
@@ -95,7 +95,7 @@ Set-ExecutionPolicy Bypass -Scope Process
 
 # Defining global script variables
 # Current script's version, the same as the version at the top in the script info section
-[datetime]$CurrentVersion = '2023.9.06'
+[datetime]$CurrentVersion = '2023.9.12'
 # Minimum OS build number required for the hardening measures used in this script
 [decimal]$Requiredbuild = '22621.2134'
 

77bae23f38a3dbdb86ae084963bd2c0292909fa5
Violet <spynetgirl@outlook.com>
2023-12-09 11:53:58 -0930

    Update PSDefaultParameterValues.ps1

--- WDACConfig/WDACConfig Module Files/CoreExt/PSDefaultParameterValues.ps1
+++ WDACConfig/WDACConfig Module Files/CoreExt/PSDefaultParameterValues.ps1
@@ -1,7 +1,5 @@
 # $PSDefaultParameterValues only get read from scope where invocation occurs
 # This is why this file is dot-sourced in every other component of the WDACConfig module at the beginning
-# With the exception of the cmdlets where ArgumentCompleters.ps1 is dot-sourced at the end
-# Because PSDefaultParameterValues.ps1 is already dot-sourced in ArgumentCompleters.ps1
 $PSDefaultParameterValues = @{
     'Invoke-WebRequest:HttpVersion'        = '3.0'
     'Invoke-WebRequest:SslProtocol'        = 'Tls12,Tls13'

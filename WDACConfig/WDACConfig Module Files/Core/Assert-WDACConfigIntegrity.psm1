ae1cfacd1d725d024aba4358780e6e4ec458e8b3
Violet <spynetgirl@outlook.com>
2023-12-26 00:40:22 -0930

    Created new cmdlet: Assert-WDACConfigIntegrity
    Used to verify the integrity of the WDACConfig module with the most secure available hashing algo: SHA2 512
    
    Will switch to SHA3 hashes that are available in .NET 8 and later once they are available in stable builds Windows. They are currently available in insider channels.

---
+++ WDACConfig/WDACConfig Module Files/Core/Assert-WDACConfigIntegrity.psm1
@@ -0,0 +1,91 @@
+function Assert-WDACConfigIntegrity {
+    [CmdletBinding()]
+    param (
+        [Alias('S')]
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.SwitchParameter]$SaveLocally,
+
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.SwitchParameter]$SkipVersionCheck
+    )
+    begin {
+        # Detecting if Verbose switch is used
+        $PSBoundParameters.Verbose.IsPresent ? ([System.Boolean]$Verbose = $true) : ([System.Boolean]$Verbose = $false) | Out-Null
+
+        # Importing the $PSDefaultParameterValues to the current session, prior to everything else
+        . "$ModuleRootPath\CoreExt\PSDefaultParameterValues.ps1"
+
+        # Importing the required sub-modules
+        Write-Verbose -Message 'Importing the required sub-modules'
+        Import-Module -FullyQualifiedName "$ModuleRootPath\Shared\Write-ColorfulText.psm1" -Force
+        Import-Module -FullyQualifiedName "$ModuleRootPath\Shared\Update-self.psm1" -Force
+
+        # if -SkipVersionCheck wasn't passed, run the updater
+        if (-NOT $SkipVersionCheck) { Update-self -InvocationStatement $MyInvocation.Statement }
+
+        # Define the output file name and the URL of the cloud CSV file
+        [System.String]$OutputFileName = 'Hashes.csv'
+        [System.Uri]$Url = 'https://raw.githubusercontent.com/HotCakeX/Harden-Windows-Security/WDACConfig-v0.2.8/WDACConfig/WDACConfig%20Module%20Files/Hashes.csv'
+
+        # Download the cloud CSV file and convert it to an array of objects
+        [System.Object[]]$CloudCSV = (Invoke-WebRequest -Uri $Url -ProgressAction SilentlyContinue).Content | ConvertFrom-Csv
+
+        # An empty array to store the final results
+        [System.Object[]]$FinalOutput = @()
+    }
+    process {
+
+        Write-Verbose -Message 'Looping through all the files'
+        foreach ($File in Get-ChildItem -Path $ModuleRootPath -Recurse -File -Force) {
+
+            if ($File.Name -eq $OutputFileName) {
+                Write-Verbose -Message "Skipping the output file: $($File.Name)"
+                continue
+            }
+
+            # Create a custom object to store the relative path, file name and the hash of the file
+            $FinalOutput += [PSCustomObject]@{
+                RelativePath = ([System.IO.Path]::GetRelativePath($ModuleRootPath, $File.FullName))
+                FileName     = $File.Name
+                FileHash     = (Get-FileHash -Path $File.FullName -Algorithm 'SHA512').Hash
+            }
+        }
+
+        if ($SaveLocally) {
+            # Export the array to a CSV file at the root of the folder
+            $FinalOutput | Export-Csv -Path (Join-Path -Path $ModuleRootPath -ChildPath $OutputFileName) -Force
+        }
+    }
+    end {
+        Write-Verbose -Message 'Comparing the local files hashes with the ones in the cloud'
+        [System.Object[]]$ComparisonResults = Compare-Object -ReferenceObject $CloudCSV -DifferenceObject $FinalOutput -Property RelativePath, FileName, FileHash | Where-Object -Property SideIndicator -EQ '=>'
+
+        if ($ComparisonResults) {
+            Write-ColorfulText -Color PinkBoldBlink -InputText 'The following files are different from the ones in the cloud:'
+            $ComparisonResults
+        }
+        else {
+            Write-ColorfulText -Color NeonGreen -InputText 'All of your local WDACConfig files are genuine.'
+        }
+    }
+    <#
+.SYNOPSIS
+    Gets the SHA512 hashes of files in the WDACConfig and compares them with the ones in the cloud and shows the differences.
+
+.DESCRIPTION
+    The Assert-WDACConfigIntegrity function scans all the files in the WDACConfig's folder and its subfolders, calculates their SHA512 hashes using the Get-FileHash cmdlet.
+    Then it downloads the cloud CSV file from the GitHub repository and compares the hashes of the local files with the ones in the cloud.
+    This way you can make sure that the files in your local WDACConfig folder are the same as the ones in the cloud and no one has tampered with them.
+
+.INPUTS
+    System.Management.Automation.SwitchParameter
+
+.OUTPUTS
+    System.String
+    System.Object[]
+
+.EXAMPLE
+    Assert-WDACConfigIntegrity
+
+#>
+}

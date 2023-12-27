ff283a3f0a9d1769d02e6298737fdcc5de5e1c0e
Violet <spynetgirl@outlook.com>
2023-12-27 02:38:35 -0930

    Code signing

--- WDACConfig/Utilities/Invoke-WDACConfig.ps1
+++ WDACConfig/Utilities/Invoke-WDACConfig.ps1
@@ -7,11 +7,15 @@
 Import-Module -FullyQualifiedName "$ScriptFilePath\..\WDACConfig Module Files\WDACConfig.psd1" -Force
 
 # To cryptographically sign the files - you should comment this section when debugging the module
+<#
+
 [System.IO.FileInfo[]]$Files = Get-ChildItem -Recurse -File -Path "$ScriptFilePath\..\WDACConfig Module Files\" -Include '*.ps1', '*.psm1', '*.psd1*'
-[System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate = Get-ChildItem -Path Cert:\CurrentUser\My -CodeSigningCert | Where-Object { $_.Thumbprint -eq 'D478B987FC25FB1786CBDF54C409F58A1753319B' }
+[System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate = Get-ChildItem -Path Cert:\CurrentUser\My -CodeSigningCert | Where-Object { $_.Thumbprint -eq '1c1c9082551b43eec17c0301bfb2f27031a4d8c8' }
 foreach ($File in $Files) {
     Set-AuthenticodeSignature -FilePath $File -Certificate $Certificate | Format-List -Property *
 }
+
+#>
 
 # Replace with any cmdlet of the WDACConfig module that is going to be debugged
 Assert-WDACConfigIntegrity -SaveLocally -Verbose

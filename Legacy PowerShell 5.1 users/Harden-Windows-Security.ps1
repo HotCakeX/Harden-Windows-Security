635951410ff69cddb1c723f5338ccda36bd529c0
Violet <spynetgirl@outlook.com>
2024-01-03 00:37:55 -0930

    Update Harden-Windows-Security.ps1

--- Legacy PowerShell 5.1 users/Harden-Windows-Security.ps1
+++ Legacy PowerShell 5.1 users/Harden-Windows-Security.ps1
@@ -1,3 +1,88 @@
+<#PSScriptInfo
+
+.VERSION 2023.12.15
+
+.GUID d435a293-c9ee-4217-8dc1-4ad2318a5770
+
+.AUTHOR HotCakeX
+
+.COMPANYNAME SpyNetGirl
+
+.COPYRIGHT 2023
+
+.TAGS Windows Hardening Security BitLocker Defender Firewall Edge Protection Baseline TLS UAC Encryption
+
+.LICENSEURI https://github.com/HotCakeX/Harden-Windows-Security/blob/main/LICENSE
+
+.PROJECTURI https://github.com/HotCakeX/Harden-Windows-Security
+
+.ICONURI https://raw.githubusercontent.com/HotCakeX/Harden-Windows-Security/main/images/PowerShellGalleryICONURI.png
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+
+#>
+
+<#
+
+.SYNOPSIS
+    Harden Windows Safely, Securely, only with Official Microsoft methods
+
+.DESCRIPTION
+
+  ⭕ You need to read the GitHub's readme page before running this: https://github.com/HotCakeX/Harden-Windows-Security
+
+  ⭕ This script is only for users that use the old PowerShell 5.1. It's highly recommended to use new PowerShell versions and the new Harden Windows Security Module that offers hardening + Auditing + Undoing hardening: https://www.powershellgallery.com/packages/Harden-Windows-Security-Module/
+
+💠 Features of this Hardening script:
+
+  ✅ Everything always stays up-to-date with the newest proactive security measures that are industry standards and scalable.
+  ✅ Everything is in plain text, nothing hidden, no 3rd party executable or pre-compiled binary is involved.
+  ✅ Doesn't remove or disable Windows functionalities against Microsoft's recommendations.
+  ✅ The script primarily uses Group policies, the Microsoft recommended way of configuring Windows. It also uses PowerShell cmdlets where Group Policies aren't available, and finally uses a few registry keys to configure security measures that can neither be configured using Group Policies nor PowerShell cmdlets. This is why the script doesn't break anything or cause unwanted behavior.
+  ✅ When a hardening measure is no longer necessary because it's applied by default by Microsoft on new builds of Windows, it will also be removed from this script in order to prevent any problems and because it won't be necessary anymore.
+  ✅ The script can be run infinite number of times, it's made in a way that it won't make any duplicate changes.
+  ✅ The script prompts for confirmation before running each hardening category and some sub-categories, so you can selectively run (or don't run) each of them.
+  ✅ Applying this script makes your PC compliant with Microsoft Security Baselines and Secured-core PC specifications (provided that you use modern hardware that supports the latest Windows security features)
+
+🛑 Note: Windows by default is secure and safe, this script does not imply nor claim otherwise. just like anything, you have to use it wisely and don't compromise yourself with reckless behavior and bad user configuration; Nothing is foolproof. this script only uses the tools and features that have already been implemented by Microsoft in Windows OS to fine-tune it towards the highest security and locked-down state, using well-documented, supported, recommended and official methods. continue reading on GitHub for comprehensive info.
+
+💠 Hardening Categories from top to bottom: (🔻Detailed info about each of them at my Github🔻)
+
+⏹ Commands that require Administrator Privileges
+  ✅ Microsoft Security Baselines
+  ✅ Microsoft 365 Apps Security Baselines
+  ✅ Microsoft Defender
+  ✅ Attack surface reduction rules
+  ✅ BitLocker Settings
+  ✅ TLS Security
+  ✅ Lock Screen
+  ✅ UAC (User Account Control)
+  ✅ Windows Firewall
+  ✅ Optional Windows Features
+  ✅ Windows Networking
+  ✅ Miscellaneous Configurations
+  ✅ Windows Update Configurations
+  ✅ Edge Browser Configurations
+  ✅ Certificate Checking Commands
+  ✅ Country IP Blocking
+⏹ Commands that don't require Administrator Privileges
+  ✅ Non-Admin Commands that only affect the current user and do not make machine-wide changes.
+
+
+🏴 If you have any questions, requests, suggestions etc. about this script, please open a new Discussion or Issue on GitHub
+
+.EXAMPLE
+
+.NOTES
+    Check out GitHub page for security recommendations: https://github.com/HotCakeX/Harden-Windows-Security
+#>
+
 # Get the execution policy for the current process
 [System.String]$CurrentExecutionPolicy = Get-ExecutionPolicy -Scope Process
 

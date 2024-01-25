35a0de807005b8d66a80e1d1c22a3851dbb73183
Violet <spynetgirl@outlook.com>
2024-01-25 23:37:36 +0200

    Improved CSV output for nested data
    The nested data such as string arrays now properly show up in the CSV output file

--- Harden-Windows-Security Module/Main files/Core/Confirm-SystemCompliance.psm1
+++ Harden-Windows-Security Module/Main files/Core/Confirm-SystemCompliance.psm1
@@ -510,7 +510,7 @@ function Confirm-SystemCompliance {
                         $NestedObjectArray += [PSCustomObject]@{
                             FriendlyName = "Process Mitigations for: $ProcessName_Target"
                             Compliant    = $False
-                            Value        = $ProcessMitigations_Applied
+                            Value        = ($ProcessMitigations_Applied -join ',')
                             Name         = "Process Mitigations for: $ProcessName_Target"
                             Category     = $CatName
                             Method       = 'Cmdlet'
@@ -526,7 +526,7 @@ function Confirm-SystemCompliance {
                         $NestedObjectArray += [PSCustomObject]@{
                             FriendlyName = "Process Mitigations for: $ProcessName_Target"
                             Compliant    = $true
-                            Value        = $ProcessMitigations_Target
+                            Value        = ($ProcessMitigations_Target -join ',')
                             Name         = "Process Mitigations for: $ProcessName_Target"
                             Category     = $CatName
                             Method       = 'Cmdlet'
@@ -893,7 +893,7 @@ function Confirm-SystemCompliance {
             $NestedObjectArray += [PSCustomObject]@{
                 FriendlyName = 'ECC Curves and their positions'
                 Compliant    = [System.Boolean]($IndividualItemResult ? $false : $True)
-                Value        = $List
+                Value        = ($List -join ',')
                 Name         = 'ECC Curves and their positions'
                 Category     = $CatName
                 Method       = 'Cmdlet'

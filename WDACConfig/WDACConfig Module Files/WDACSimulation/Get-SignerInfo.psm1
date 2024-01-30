405630ce8fb1f8ba9bd1ae2d9a72209a9468e7f6
Violet <spynetgirl@outlook.com>
2024-01-31 00:39:28 +0200

    Added info for wellknown cert roots

--- WDACConfig/WDACConfig Module Files/WDACSimulation/Get-SignerInfo.psm1
+++ WDACConfig/WDACConfig Module Files/WDACSimulation/Get-SignerInfo.psm1
@@ -34,6 +34,67 @@ Function Get-SignerInfo {
 
         # Loop through each Signer node and extract the information
         foreach ($Signer in $Signers) {
+            
+            # Replacing Wellknown root IDs with their corresponding TBS values
+            if ($Signer.CertRoot.Value -in ('03', '04', '05', '06', '07', '09', '0A', '0E', '0G', '0H', '0I')) {
+                switch ($Signer.CertRoot.Value) {
+                    '03' { 
+                        $Signer.CertRoot.Value = 'D67576F5521D1CCAB52E9215E0F9F743'
+                        $Signer.Name = 'Microsoft Authenticode(tm) Root Authority'
+                        break 
+                    }
+                    '04' {
+                        $Signer.CertRoot.Value = '8B3C3087B7056F5EC5DDBA91A1B901F0'
+                        $Signer.Name = 'Microsoft Root Authority'                   
+                        break 
+                    }
+                    '05' {
+                        $Signer.CertRoot.Value = '391BE92883D52509155BFEAE27B9BD340170B76B'
+                        $Signer.Name = 'Microsoft Root Certificate Authority'
+                        break 
+                    }
+                    '06' { 
+                        $Signer.CertRoot.Value = '121AF4B922A74247EA49DF50DE37609CC1451A1FE06B2CB7E1E079B492BD8195'
+                        $Signer.Name = 'Microsoft Code Signing PCA 2010'
+                        break 
+                    }
+                    '07' {
+                        $Signer.CertRoot.Value = 'F6F717A43AD9ABDDC8CEFDDE1C505462535E7D1307E630F9544A2D14FE8BF26E'
+                        $Signer.Name = 'Microsoft Code Signing PCA 2011'
+                        break 
+                    }
+                    '09' {
+                        $Signer.CertRoot.Value = '09CBAFBD98E81B4D6BAAAB32B8B2F5D7'
+                        $Signer.Name = 'Microsoft Test Root Authority'
+                        break 
+                    }
+                    '0A' { 
+                        $Signer.CertRoot.Value = '7A4D9890B0F9006A6F77472D50D83CA54975FCC2B7EA0563490134E19B78782A'
+                        $Signer.Name = 'Microsoft Testing Root Certificate Authority 2010'
+                        break 
+                    }
+                    '0E' { 
+                        $Signer.CertRoot.Value = 'ED55F82E1444F79CA9DCE826846FDC4E0EA3859E3D26EFEF412D2FFF0C7C8E6C'
+                        $Signer.Name = 'Microsoft Development Root Certificate Authority 2014'
+                        break 
+                    }
+                    '0G' {
+                        $Signer.CertRoot.Value = '68D221D720E975DB5CD14B24F2970F86A5B8605A2A1BC784A17B83F7CF500A70EB177CE228273B8540A800178F23EAC8'
+                        $Signer.Name = 'Microsoft ECC Testing Root Certificate Authority 2017'
+                        break 
+                    }
+                    '0H' {
+                        $Signer.CertRoot.Value = '214592CB01B59104195F80AF2886DBF85771AF42A3821D104BF18F415158C49CBC233511672CD6C432351AC9228E3E75'
+                        $Signer.Name = 'Microsoft ECC Development Root Certificate Authority 2018'
+                        break 
+                    }
+                    '0I' {
+                        $Signer.CertRoot.Value = '32991981BF1575A1A5303BB93A381723EA346B9EC130FDB596A75BA1D7CE0B0A06570BB985D25841E23BE944E8FF118F'
+                        $Signer.Name = 'Microsoft ECC Product Root Certificate Authority 2018'
+                        break 
+                    }
+                }               
+            }
 
             # Create a new instance of the Signer class in the WDACConfig Namespace
             [WDACConfig.Signer]$SignerObj = New-Object -TypeName WDACConfig.Signer -ArgumentList ($Signer.ID, $Signer.Name, $Signer.CertRoot.Value, $Signer.CertPublisher.Value)

12adadce804b0ec870aeab72df66675feb337779
Violet <spynetgirl@outlook.com>
2024-02-20 01:12:13 +0200

    Modified namespace

--- WDACConfig/WDACConfig Module Files/Shared/Get-GlobalRootDrives.psm1
+++ WDACConfig/WDACConfig Module Files/Shared/Get-GlobalRootDrives.psm1
@@ -17,7 +17,7 @@ Function Get-GlobalRootDrives {
     . "$ModuleRootPath\CoreExt\PSDefaultParameterValues.ps1"
 
     # Import the kernel32.dll functions using P/Invoke if they don't exist
-    if (-NOT ('PInvoke.Win32Utils' -as [System.Type]) ) {
+    if (-NOT ('WDACConfig.Win32Utils' -as [System.Type]) ) {
         Add-Type -Path "$ModuleRootPath\C#\Kernel32dll.cs"
     }
 
@@ -29,16 +29,16 @@ Function Get-GlobalRootDrives {
     [System.Text.StringBuilder]$SbMountPoint = New-Object -TypeName System.Text.StringBuilder -ArgumentList ($Max, $Max)
 
     # Find the first volume in the system and get a handle to it
-    [System.IntPtr]$VolumeHandle = [PInvoke.Win32Utils]::FindFirstVolume($SbVolumeName, $Max)
+    [System.IntPtr]$VolumeHandle = [WDACConfig.Win32Utils]::FindFirstVolume($SbVolumeName, $Max)
 
     # Loop through all the volumes in the system
     do {
         # Get the volume name as a string
         [System.String]$Volume = $SbVolumeName.toString()
         # Get the mount point for the volume, if any
-        [System.Boolean]$unused = [PInvoke.Win32Utils]::GetVolumePathNamesForVolumeNameW($Volume, $SbMountPoint, $Max, [System.Management.Automation.PSReference]$lpcchReturnLength)
+        [System.Boolean]$unused = [WDACConfig.Win32Utils]::GetVolumePathNamesForVolumeNameW($Volume, $SbMountPoint, $Max, [System.Management.Automation.PSReference]$lpcchReturnLength)
         # Get the device path for the volume, if any
-        [System.UInt32]$ReturnLength = [PInvoke.Win32Utils]::QueryDosDevice($Volume.Substring(4, $Volume.Length - 1 - 4), $SbPathName, [System.UInt32]$Max)
+        [System.UInt32]$ReturnLength = [WDACConfig.Win32Utils]::QueryDosDevice($Volume.Substring(4, $Volume.Length - 1 - 4), $SbPathName, [System.UInt32]$Max)
 
         # If the device path is found, create a custom object with the drive mapping information
         if ($ReturnLength) {
@@ -55,7 +55,7 @@ Function Get-GlobalRootDrives {
             Write-Output -InputObject 'No mountpoint found for: ' + $Volume
         }
         # Find the next volume in the system and repeat the loop
-    } while ([PInvoke.Win32Utils]::FindNextVolume([System.IntPtr]$VolumeHandle, $SbVolumeName, $Max))
+    } while ([WDACConfig.Win32Utils]::FindNextVolume([System.IntPtr]$VolumeHandle, $SbVolumeName, $Max))
 
 }
 

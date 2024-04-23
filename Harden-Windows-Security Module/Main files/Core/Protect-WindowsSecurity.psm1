298b2febb95cc835166c561754f50d25fe87b0a2
Violet <spynetgirl@outlook.com>
2024-04-24 01:05:09 +0300

    Update Protect-WindowsSecurity.psm1

--- Harden-Windows-Security Module/Main files/Core/Protect-WindowsSecurity.psm1
+++ Harden-Windows-Security Module/Main files/Core/Protect-WindowsSecurity.psm1
@@ -4166,7 +4166,7 @@ End time: $(Get-Date)
                     $SyncHash.Error = $Error
                 })
 
-            $GUIAsyncObject = $GUIPowerShell.Invoke()
+                [System.Void]$GUIPowerShell.Invoke()
 
             if ($SyncHash.Error) {
                 $SyncHash.Error | ForEach-Object -Process {

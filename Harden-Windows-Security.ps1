2091e3d520ee92883c18de60fd5aca7ffd862f8b
Violet Hansen <spynetgirl@outlook.com>
2024-07-04 15:58:15 +0300

    improved bootstrapping script

--- Harden-Windows-Security.ps1
+++ Harden-Windows-Security.ps1
@@ -9,51 +9,61 @@ Function P {
     Param (
         [System.Management.Automation.SwitchParameter]$G
     )
-    [System.Boolean]$CommandRan = $false
-    if ($G) {
-        [System.Management.Automation.ScriptBlock]$Command = {
-            (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/HotCakeX/Harden-Windows-Security/main/Harden-Windows-Security%20Module/Main%20files/Core/Protect-WindowsSecurity.psm1') + 'P -G' | Invoke-Expression
+
+    if ($PSVersionTable.PSEdition -eq 'Desktop') {
+
+        if (!(Get-Command -Name 'pwsh.exe')) {
+            try {
+                Write-Verbose -Verbose -Message 'Trying to Install PowerShell Core using Winget because it could not be found' -Verbose
+                $null = Winget install 9MZ1SNWT0N5D --accept-package-agreements --accept-source-agreements
+            }
+            catch {
+                Write-Verbose -Verbose -Message 'Failed to Install PowerShell Core using Winget' -Verbose
+                $progressPreference = 'silentlyContinue'
+                Write-Verbose -Verbose -Message 'Downloading WinGet and its dependencies...' -Verbose
+                # https://learn.microsoft.com/en-us/windows/package-manager/winget/#install-winget-on-windows-sandbox
+                Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+                Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+                Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.8.x64.appx'
+
+                Add-AppxPackage -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+                Add-AppxPackage -Path 'Microsoft.UI.Xaml.2.8.x64.appx'
+                Add-AppxPackage -Path 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+
+                Remove-Item -Path 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -Force
+                Remove-Item -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx' -Force
+                Remove-Item -Path 'Microsoft.UI.Xaml.2.8.x64.appx' -Force
+            }
+
+            Write-Verbose -Verbose -Message 'Trying to Install PowerShell Core using Winget again' -Verbose
+            # https://apps.microsoft.com/detail/9mz1snwt0n5d
+            $null = Winget install 9MZ1SNWT0N5D --accept-package-agreements --accept-source-agreements
+        }
+        else {
+            Write-Verbose -Verbose -Message 'Trying to run the command in PowerShell Core'
+            pwsh.exe -NoLogo -NoExit -Command {
+                Install-Module -Name 'Harden-Windows-Security-Module' -Force
+                Protect-WindowsSecurity
+            }
         }
     }
     else {
-        [System.Management.Automation.ScriptBlock]$Command = {
-            (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/HotCakeX/Harden-Windows-Security/main/Harden-Windows-Security%20Module/Main%20files/Core/Protect-WindowsSecurity.psm1') + 'P' | Invoke-Expression
+        if (($PSVersionTable.PSVersion) -lt '7.4.2') {
+            if ($PSHome -like '*\Program Files\WindowsApps\*') {
+                throw 'Update PowerShell from Microsoft Store because you are using an older version'
+            }
+            else {
+                throw "Install newer version of PowerShell and try again`nhttps://github.com/PowerShell/PowerShell/releases/latest"
+            }
+        }
+        else {
+            Write-Verbose -Verbose -Message 'Trying to run the command in PowerShell Core'
+            pwsh.exe -NoLogo -NoExit -Command {
+                Install-Module -Name 'Harden-Windows-Security-Module' -Force
+                Protect-WindowsSecurity
+            }
         }
     }
-    Try {
-        pwsh.exe -NoLogo -NoExit -command $Command
-        $CommandRan = $true
-    }
-    Catch {
-        Write-Verbose -Verbose -Message 'Failed to run the command in PowerShell Core'
-    }
-    if (-NOT $CommandRan) {
-        try {
-            Write-Verbose -Verbose -Message 'Trying to Install PowerShell Core using Winget'
-            Winget install 9MZ1SNWT0N5D --accept-package-agreements --accept-source-agreements | Out-Null
-        }
-        catch {
-            Write-Verbose -Verbose -Message 'Failed to Install PowerShell Core using Winget'
-            $progressPreference = 'silentlyContinue'
-            Write-Verbose -Verbose -Message 'Downloading WinGet and its dependencies...'
-            # https://learn.microsoft.com/en-us/windows/package-manager/winget/#install-winget-on-windows-sandbox
-            Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-            Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
-            Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.8.x64.appx'
-
-            Add-AppxPackage -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
-            Add-AppxPackage -Path 'Microsoft.UI.Xaml.2.8.x64.appx'
-            Add-AppxPackage -Path 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-
-            Remove-Item -Path 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -Force
-            Remove-Item -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx' -Force
-            Remove-Item -Path 'Microsoft.UI.Xaml.2.8.x64.appx' -Force
-        }
-    }
-    Write-Verbose -Verbose -Message 'Trying to Install PowerShell Core using Winget'
-    # https://apps.microsoft.com/detail/9mz1snwt0n5d
-    Winget install 9MZ1SNWT0N5D --accept-package-agreements --accept-source-agreements | Out-Null
-
-    Write-Verbose -Verbose -Message 'Trying to run the command in PowerShell Core'
-    pwsh.exe -NoLogo -NoExit -command $Command
 }
+
+P

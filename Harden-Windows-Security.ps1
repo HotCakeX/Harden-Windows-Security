d1261e48b33aa77933c9dbd8b2acdcbf9458aa6f
Violet Hansen <spynetgirl@outlook.com>
2024-07-11 01:20:37 +0300

    Improved Bootstrapper
    The bootstrapper is now more feature rich, being able to bootstrap the module right after installing Windows OS

--- Harden-Windows-Security.ps1
+++ Harden-Windows-Security.ps1
@@ -1,83 +1,157 @@
 Function P {
-    <#
-    .SYNOPSIS
-        Effortlessly starts the Harden Windows Security module on any system
-        if necessary, installs PowerShell core using Winget from Microsoft Store
-        If Winget is not present, Installs it along with its dependencies on the system
-        Can start from Windows PowerShell too
-    #>
-    Param (
-        [System.Management.Automation.SwitchParameter]$G
-    )
+    [CmdletBinding()]
+    param([switch]$G)
+    begin {
+        $ErrorActionPreference = 'Stop'
+        Set-ExecutionPolicy -ExecutionPolicy 'Unrestricted' -Scope 'Process' -Force
+        [System.Boolean]$WingetSourceUpdated = $false
+        [System.Boolean]$PSInstalled = $false
+        [System.String]$PSDownloadURLMSIX = 'https://github.com/PowerShell/PowerShell/releases/download/v7.4.3/PowerShell-7.4.3-Win.msixbundle'
+        $UserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
+        $User = Get-LocalUser | Where-Object -FilterScript { $_.SID -eq $UserSID }
 
-    Set-ExecutionPolicy -ExecutionPolicy 'Unrestricted' -Scope 'Process' -Force
-
-    if ($PSVersionTable.PSEdition -eq 'Desktop') {
-
-        if (!(Get-Command -Name 'pwsh.exe' -ErrorAction Ignore)) {
-            try {
-                Write-Verbose -Message 'Trying to Install PowerShell Core using Winget because it could not be found' -Verbose
-                $null = Winget install 9MZ1SNWT0N5D --accept-package-agreements --accept-source-agreements
-            }
-            catch {
-
-                # Change location to temp because Windows PowerShell's default dir is System32 and if running as non-admin cannot be used for download location
-                Push-Location -Path ([System.IO.Path]::GetTempPath())
-
-                Write-Verbose -Message 'Failed to Install PowerShell Core using Winget' -Verbose
-                $progressPreference = 'silentlyContinue'
-                Write-Verbose -Message 'Downloading WinGet and its dependencies...' -Verbose
-                # https://learn.microsoft.com/en-us/windows/package-manager/winget/#install-winget-on-windows-sandbox
-                Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-                Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
-                Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.8.x64.appx'
-
-                Add-AppxPackage -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
-                Add-AppxPackage -Path 'Microsoft.UI.Xaml.2.8.x64.appx'
-                Add-AppxPackage -Path 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-
-                Remove-Item -Path 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -Force
-                Remove-Item -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx' -Force
-                Remove-Item -Path 'Microsoft.UI.Xaml.2.8.x64.appx' -Force
-
-                Pop-Location
-
-                Write-Verbose -Message 'Trying to Install PowerShell Core using Winget again' -Verbose
-                # https://apps.microsoft.com/detail/9mz1snwt0n5d
-                $null = Winget install 9MZ1SNWT0N5D --accept-package-agreements --accept-source-agreements
-            }
+        Function Install-StoreSource {
+            # https://apps.microsoft.com/detail/9mz1snwt0n5d
+            Write-Verbose -Message 'Microsoft account detected, using Microsoft Store source for PowerShell installation through Winget'
+            $null = Winget install --id 9MZ1SNWT0N5D --accept-package-agreements --accept-source-agreements --source msstore
+        }
+        Function Install-GitHubMSIX {
+            Write-Verbose -Message 'Downloading and Installing PowerShell directly from GitHub using MSIX file'
+            Invoke-WebRequest -Uri $PSDownloadURLMSIX -OutFile 'PowerShell.msixbundle'
+            Add-AppxPackage -Path 'PowerShell.msixbundle'
         }
     }
-    else {
-        if (($PSVersionTable.PSVersion) -lt '7.4.2') {
-            if ($PSHome -like '*\Program Files\WindowsApps\*') {
-                throw 'Update PowerShell from Microsoft Store because you are using an older version'
+    process {
+        if ($PSVersionTable.PSEdition -eq 'Desktop') {
+            if (!(Get-Command -Name 'pwsh.exe' -ErrorAction Ignore)) {
+                try {
+                    Write-Verbose -Message 'Trying to Install PowerShell Core using Winget because it could not be found on the system' -Verbose
+                    Write-Verbose -Message 'Updating Winget source...'
+                    $null = winget source update
+                    $WingetSourceUpdated = $true
+
+                    if ($User.PrincipalSource -eq 'MicrosoftAccount') {
+                        Install-StoreSource
+                    }
+                    else {
+                        Write-Verbose -Message 'Local account detected, cannot install PowerShell Core from Microsoft Store using Winget and msstore as the source'
+                        Throw
+                    }
+
+                    if ($LASTEXITCODE -ne 0) {
+                        Write-Verbose -Message "Failed to Install PowerShell Core using Winget: $LASTEXITCODE"
+                        throw
+                    }
+                    $PSInstalled = $true
+                }
+                catch {
+                    try {
+                        # Change location to temp because Windows PowerShell's default dir is System32 and if running as non-admin cannot be used for download location
+                        Push-Location -Path ([System.IO.Path]::GetTempPath())
+
+                        Write-Verbose -Message 'Failed to Install PowerShell Core using Winget' -Verbose
+
+                        $ProgressPreference = 'silentlyContinue'
+                        Write-Verbose -Message 'Downloading WinGet and its dependencies...' -Verbose
+                        # https://learn.microsoft.com/en-us/windows/package-manager/winget/#install-winget-on-windows-sandbox
+                        Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+                        Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+                        Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.8.x64.appx'
+
+                        Add-AppxPackage -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+                        Add-AppxPackage -Path 'Microsoft.UI.Xaml.2.8.x64.appx'
+                        Add-AppxPackage -Path 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+
+                        Remove-Item -Path 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -Force
+                        Remove-Item -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx' -Force
+                        Remove-Item -Path 'Microsoft.UI.Xaml.2.8.x64.appx' -Force
+
+                        Pop-Location
+
+                        Write-Verbose -Message 'Trying to Install PowerShell Core using Winget again after installing Winget' -Verbose
+
+                        if (!$WingetSourceUpdated) {
+                            Write-Verbose -Message 'Updating Winget source...'
+                            $null = winget source update
+                        }
+
+                        if ($User.PrincipalSource -eq 'MicrosoftAccount') {
+                            Install-StoreSource
+                        }
+                        else {
+                            Write-Verbose -Message 'Local account detected, cannot install PowerShell Core from Microsoft Store using Winget and msstore as the source'
+                            Throw
+                        }
+                        if ($LASTEXITCODE -ne 0) {
+                            Write-Verbose -Message "Failed to Install PowerShell Core using Winget: $LASTEXITCODE"
+                            throw
+                        }
+                        $PSInstalled = $true
+                    }
+                    catch {
+                        try {
+                            Push-Location -Path ([System.IO.Path]::GetTempPath())
+                            Install-GitHubMSIX
+                            $PSInstalled = $true
+                        }
+                        catch {
+                            throw
+                        }
+                        finally {
+                            try {
+                                Remove-Item -Path 'PowerShell.msixbundle' -Force
+                            }
+                            catch {}
+                            Pop-Location
+                        }
+                    }
+                }
             }
             else {
-                throw "Install newer version of PowerShell and try again`nhttps://github.com/PowerShell/PowerShell/releases/latest"
+                $PSInstalled = $true
             }
         }
-    }
+        else {
+            if (($PSVersionTable.PSVersion) -lt '7.4.2') {
 
-    Write-Verbose -Message 'Trying to run the command in PowerShell Core'
-    if ($G) {
-        pwsh.exe -NoLogo -NoExit -Command {
-            Set-ExecutionPolicy -ExecutionPolicy 'Unrestricted' -Scope 'Process' -Force
-            if (!(Get-Module -ListAvailable -Name 'Harden-Windows-Security-Module' -ErrorAction Ignore)) {
-                Write-Verbose -Message 'Installing the Harden Windows Security Module because it could not be found'
-                Install-Module -Name 'Harden-Windows-Security-Module' -Force
+                Write-Verbose -Message 'PowerShell version is older than the required version, downloading and installing the latest version' -Verbose
+                if ($PSHome -notlike '*\Program Files\WindowsApps\*') {
+                    Write-Verbose -Message "Your current PowerShell is not installed from Microsoft Store, but the version being automatically downloaded is MSIX version`nYou can remove the old version later from the Windows Settings" -Verbose
+                }
+
+                try {
+                    Install-GitHubMSIX
+                    $PSInstalled = $true
+                }
+                catch {
+                    throw 'Update PowerShell from Microsoft Store because you are using an older version and could not automatically update it'
+                }
+                finally {
+                    try {
+                        Remove-Item -Path 'PowerShell.msixbundle' -Force
+                    }
+                    catch {}
+                }
             }
-            Protect-WindowsSecurity -GUI
+            else {
+                $PSInstalled = $true
+            }
         }
     }
-    else {
-        pwsh.exe -NoLogo -NoExit -Command {
-            Set-ExecutionPolicy -ExecutionPolicy 'Unrestricted' -Scope 'Process' -Force
-            if (!(Get-Module -ListAvailable -Name 'Harden-Windows-Security-Module' -ErrorAction Ignore)) {
-                Write-Verbose -Message 'Installing the Harden Windows Security Module because it could not be found'
-                Install-Module -Name 'Harden-Windows-Security-Module' -Force
+    end {
+        if ($PSInstalled) {
+            Write-Verbose -Message 'Trying to run the command in PowerShell Core'
+            pwsh.exe -NoLogo -NoExit -Command {
+                Set-ExecutionPolicy -ExecutionPolicy 'Unrestricted' -Scope 'Process' -Force
+                if (!(Get-Module -ListAvailable -Name 'Harden-Windows-Security-Module' -ErrorAction Ignore)) {
+                    Write-Verbose -Message 'Installing the Harden Windows Security Module because it could not be found' -Verbose
+                    Install-Module -Name 'Harden-Windows-Security-Module' -Force
+                }
+                Protect-WindowsSecurity -GUI
             }
-            Protect-WindowsSecurity
+        }
+        else {
+            throw 'Failed to automatically Install PowerShell Core after exhausting all options'
         }
     }
 }

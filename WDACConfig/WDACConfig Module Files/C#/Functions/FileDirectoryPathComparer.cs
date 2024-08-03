37925d15a85c4640a8f046afeadf45eaf21017e2
Violet Hansen <spynetgirl@outlook.com>
2024-08-03 11:12:17 +0300

    Update FileDirectoryPathComparer.cs

--- WDACConfig/WDACConfig Module Files/C#/Functions/FileDirectoryPathComparer.cs
+++ WDACConfig/WDACConfig Module Files/C#/Functions/FileDirectoryPathComparer.cs
@@ -6,17 +6,24 @@ namespace WDACConfig
 {
     public class FileDirectoryPathComparer
     {
-        // Method that takes 2 arrays, one contains file paths and the other contains folder paths. It checks them and returns the unique file paths
-        // that are not in any of the folder paths. Performs this check recursively too so works if a filepath is in a sub-directory of a folder path.
-        // It works even if the file paths or folder paths are non-existent/deleted, but they still need to be valid file/folder paths.
+        /// <summary>
+        /// Method that takes 2 arrays, one containing file paths and the other containing folder paths. 
+        /// It checks them and returns the unique file paths that are not in any of the folder paths.
+        /// Performs this check recursively, so it works if a filepath is in a sub-directory of a folder path.
+        /// It works even if the file paths or folder paths are non-existent/deleted, but they still need to be valid file/folder paths.
+        /// </summary>
+        /// <param name="directoryPaths">Array of directory paths</param>
+        /// <param name="filePaths">Array of file paths</param>
+        /// <returns>List of unique file paths that don't reside in any of the directory paths (or their sub-directory paths)</returns>
         public static List<string> TestFilePath(string[] directoryPaths, string[] filePaths)
         {
-            HashSet<string> output = new HashSet<string>();
+            HashSet<string> output = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
             // Loop through each file path
             foreach (string file in filePaths)
             {
                 bool isInDirectory = false;
+
                 // Loop through each directory path
                 foreach (string directory in directoryPaths)
                 {
@@ -28,13 +35,15 @@ namespace WDACConfig
                         break;
                     }
                 }
+
                 // Output the file path if it is not inside any of the directory paths
                 if (!isInDirectory)
                 {
                     output.Add(file);
                 }
             }
-            // return the unique file paths that don't reside in any of the directory paths (or their sub-directory paths)
+
+            // Return the unique file paths that don't reside in any of the directory paths (or their sub-directory paths)
             return new List<string>(output);
         }
     }

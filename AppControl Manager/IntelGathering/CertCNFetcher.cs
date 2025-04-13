a91965481c371c9e40052f823c0a93995b8c167c
Violet Hansen <spynetgirl@outlook.com>
2025-04-13 23:12:30 +0330

    Fixed an issue when more than 1 cert with the same CN exists (#697)
    Fixed this issue: https://github.com/HotCakeX/Harden-Windows-Security/issues/693

--- AppControl Manager/IntelGathering/CertCNFetcher.cs
+++ AppControl Manager/IntelGathering/CertCNFetcher.cs
@@ -52,7 +52,9 @@ internal static class CertCNFetcher
 					// Add the CN to the output set and warn if there is already CN with the same name in the HashSet
 					if (!output.Add(cn))
 					{
-						throw new InvalidOperationException($"There are more than 1 certificates with the common name '{cn}' in the Personal certificate store of the Current User. Delete one of them if you want to use it.");
+						Logger.Write($"There are more than 1 certificates with the common name '{cn}' in the Personal certificate store of the Current User. Only certificates without duplicate common names can be used at the moment.");
+
+						_ = output.Remove(cn);
 					}
 				}
 			}

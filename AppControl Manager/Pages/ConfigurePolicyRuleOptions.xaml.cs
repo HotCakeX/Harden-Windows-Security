cb452b1a5a300c610234c04911d68142af058bd2
Violet Hansen <spynetgirl@outlook.com>
2025-06-01 18:14:34 +0300

    f

--- AppControl Manager/Pages/ConfigurePolicyRuleOptions.xaml.cs
+++ AppControl Manager/Pages/ConfigurePolicyRuleOptions.xaml.cs
@@ -16,16 +16,9 @@
 //
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using AppControlManager.Main;
 using AppControlManager.Others;
-using AppControlManager.SiPolicy;
 using AppControlManager.ViewModels;
 using AppControlManager.WindowComponents;
-using CommunityToolkit.WinUI.Controls;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -43,20 +36,11 @@ internal sealed partial class ConfigurePolicyRuleOptions : Page, IAnimatedIconsM
 	private AppSettings.Main AppSettings { get; } = App.AppHost.Services.GetRequiredService<AppSettings.Main>();
 	private SidebarVM sideBarVM { get; } = App.AppHost.Services.GetRequiredService<SidebarVM>();
 
-	/// <summary>
-	/// Initializes the ConfigurePolicyRuleOptions class, sets up navigation caching, binds the data context, and generates
-	/// settings cards.
-	/// </summary>
 	internal ConfigurePolicyRuleOptions()
 	{
 		this.InitializeComponent();
-
-		this.NavigationCacheMode = NavigationCacheMode.Required;
-
+		this.NavigationCacheMode = NavigationCacheMode.Disabled;
 		this.DataContext = ViewModel;
-
-		// Call the method to generate SettingsCards dynamically
-		GenerateSettingsCards();
 	}
 
 	#region Augmentation Interface
@@ -81,10 +65,10 @@ internal sealed partial class ConfigurePolicyRuleOptions : Page, IAnimatedIconsM
 			PickPolicyFileButton_FlyOut.ShowAt(PickPolicyFileButton);
 			ViewModel.SelectedFilePath = MainWindowVM.SidebarBasePolicyPathTextBoxTextStatic;
 
-			await LoadPolicyOptionsFromXML(ViewModel.SelectedFilePath);
+			await ViewModel.LoadPolicyOptionsFromXML(ViewModel.SelectedFilePath);
 
 			// Expand the settings expander when user selects a policy
-			PolicyRuleExpander.IsExpanded = true;
+			ViewModel.SettingsExpanderIsExpanded = true;
 		}
 		catch (Exception ex)
 		{
@@ -94,320 +78,4 @@ internal sealed partial class ConfigurePolicyRuleOptions : Page, IAnimatedIconsM
 
 	#endregion
 
-	/// <summary>
-	/// Method to dynamically create SettingsCards based on the dictionary keys
-	/// </summary>
-	private void GenerateSettingsCards()
-	{
-		foreach (KeyValuePair<string, string> key in ViewModel.RuleOptions)
-		{
-			// Create a new SettingsCard
-			SettingsCard settingsCard = new()
-			{
-				IsClickEnabled = true,
-				IsActionIconVisible = false,
-				Header = key.Key,
-				Description = key.Value
-			};
-
-			ToolTip toolTip = new()
-			{
-				Content = key.Value,
-				HorizontalAlignment = HorizontalAlignment.Center,
-				VerticalAlignment = VerticalAlignment.Center
-			};
-
-			// Attach the tooltip to the settings card
-			ToolTipService.SetToolTip(settingsCard, toolTip);
-
-			// Create a new CheckBox
-			CheckBox checkBox = new()
-			{
-				Tag = key.Key,
-				HorizontalAlignment = HorizontalAlignment.Right
-			};
-
-			// Add the CheckBox to the SettingsCard
-			settingsCard.Content = checkBox;
-
-			// Attach click event to the SettingsCard so click/tap on settings card will be relayed to the check box
-			settingsCard.Click += (sender, e) =>
-			{
-				checkBox.IsChecked = !checkBox.IsChecked;
-			};
-
-			// Add the SettingsCard to the SettingsExpander.Items collection
-			PolicyRuleExpander.Items.Add(settingsCard);
-		}
-	}
-
-	/// <summary>
-	/// Event handler for the browse button
-	/// </summary>
-	private async void PickPolicyFileButton_Click()
-	{
-		try
-		{
-
-			string? selectedFile = FileDialogHelper.ShowFilePickerDialog(GlobalVars.XMLFilePickerFilter);
-
-			if (!string.IsNullOrWhiteSpace(selectedFile))
-			{
-				ViewModel.SelectedFilePath = selectedFile;
-
-				// Expand the settings expander when user selects a policy
-				PolicyRuleExpander.IsExpanded = true;
-
-				// Load the policy options from the XML and update the UI
-				await LoadPolicyOptionsFromXML(selectedFile);
-			}
-		}
-		catch (Exception ex)
-		{
-			ViewModel.MainInfoBar.WriteError(ex);
-		}
-	}
-
-	/// <summary>
-	/// When the XML policy file is selected by the user, get its rule options and check/uncheck the check boxes in the UI accordingly
-	/// </summary>
-	/// <param name="filePath"></param>
-	private async Task LoadPolicyOptionsFromXML(string? filePath)
-	{
-		try
-		{
-			SiPolicy.SiPolicy policyObj = null!;
-
-			await Task.Run(() =>
-			{
-				policyObj = Management.Initialize(filePath, null);
-			});
-
-			// All the Policy OptionTypes in the selected XML file
-			IEnumerable<OptionType> policyRules = policyObj.Rules.Select(x => x.Item);
-
-			// Iterate through UI checkboxes and update their state
-			foreach (var item in PolicyRuleExpander.Items)
-			{
-				if (item is SettingsCard settingsCard && settingsCard.Content is CheckBox checkBox)
-				{
-					// Get the tag of the checkbox
-					string key = checkBox.Tag.ToString()!;
-
-					if (policyRules.Contains(CustomDeserialization.ConvertStringToOptionType(key)))
-					{
-						checkBox.IsChecked = true;
-					}
-					else
-					{
-						checkBox.IsChecked = false;
-					}
-				}
-			}
-		}
-		catch (Exception ex)
-		{
-			ViewModel.MainInfoBar.WriteError(ex);
-		}
-	}
-
-	/// <summary>
-	/// Event handler for when the Apply button is pressed
-	/// </summary>
-	private async void ApplyTheChangesButton_Click()
-	{
-
-		try
-		{
-			ManageButtonStates(false);
-			MainTeachingTip.IsOpen = false;
-
-			if (string.IsNullOrWhiteSpace(ViewModel.SelectedFilePath))
-			{
-				MainTeachingTip.IsOpen = true;
-				MainTeachingTip.Subtitle = GlobalVars.Rizz.GetString("SelectPolicyFileBeforeAddingOptions");
-				return;
-			}
-
-			// Gather selected rules to add
-			OptionType[] selectedOptions = [.. GetSelectedPolicyRuleOptions()];
-
-			await Task.Run(() =>
-			{
-				CiRuleOptions.Set(ViewModel.SelectedFilePath, rulesToAdd: selectedOptions, RemoveAll: true);
-			});
-
-			if (DeployAfterApplyingToggleButton.IsChecked == true)
-			{
-				await Task.Run(() =>
-				{
-					DirectoryInfo stagingArea = StagingArea.NewStagingArea("ConfigurePolicyRuleOptionsDeployment");
-
-					string cipPath = Path.Combine(stagingArea.FullName, $"{Path.GetFileName(ViewModel.SelectedFilePath)}.cip");
-
-					SiPolicy.SiPolicy policyObj = Management.Initialize(ViewModel.SelectedFilePath, null);
-
-					if (!policyObj.Rules.Any(x => x.Item is OptionType.EnabledUnsignedSystemIntegrityPolicy))
-					{
-						_ = DispatcherQueue.TryEnqueue(() =>
-						{
-							MainTeachingTip.IsOpen = true;
-							MainTeachingTip.Subtitle = GlobalVars.Rizz.GetString("TeachingTipSubtitlePolicyRequiresSigning");
-						});
-
-						return;
-					}
-
-					Management.ConvertXMLToBinary(ViewModel.SelectedFilePath, null, cipPath);
-
-					// If a base policy is being deployed, ensure it's supplemental policy for AppControl Manager also gets deployed
-					if (SupplementalForSelf.IsEligible(policyObj, ViewModel.SelectedFilePath))
-						SupplementalForSelf.Deploy(stagingArea.FullName, policyObj.PolicyID);
-
-					CiToolHelper.UpdatePolicy(cipPath);
-				});
-			}
-		}
-		catch (Exception ex)
-		{
-			ViewModel.MainInfoBar.WriteError(ex);
-		}
-		finally
-		{
-			ManageButtonStates(true);
-		}
-	}
-
-	/// <summary>
-	/// Event handler for the Set button click in the PolicyTemplate section
-	/// </summary>
-	private async void SetPolicyTemplate_Click()
-	{
-		try
-		{
-			ManageButtonStates(false);
-			MainTeachingTip.IsOpen = false;
-
-			if (string.IsNullOrWhiteSpace(ViewModel.SelectedFilePath))
-			{
-				MainTeachingTip.IsOpen = true;
-				MainTeachingTip.Subtitle = GlobalVars.Rizz.GetString("SelectPolicyFileBeforeSettingTemplate");
-				return;
-			}
-
-			// Retrieve the selected item from the ComboBox
-			string selectedItem = (string)PolicyTemplatesComboBox.SelectedItem;
-
-			// Convert the ComboBoxItem content to the corresponding PolicyTemplate enum value
-			CiRuleOptions.PolicyTemplate template = Enum.Parse<CiRuleOptions.PolicyTemplate>(selectedItem);
-
-			// Call the Set method with only the filePath and template parameters
-			await Task.Run(() =>
-			{
-				CiRuleOptions.Set(ViewModel.SelectedFilePath, template: template);
-			});
-
-			// Refresh the UI check boxes
-			await LoadPolicyOptionsFromXML(ViewModel.SelectedFilePath);
-		}
-		catch (Exception ex)
-		{
-			ViewModel.MainInfoBar.WriteError(ex);
-		}
-		finally
-		{
-			ManageButtonStates(true);
-		}
-	}
-
-	/// <summary>
-	/// Manages buttons' disablement/enablement
-	/// </summary>
-	/// <param name="Enable"></param>
-	private void ManageButtonStates(bool Enable)
-	{
-		SetPolicyTemplate.IsEnabled = Enable;
-		RefreshRuleOptionsState.IsEnabled = Enable;
-		ApplyTheChangesButton.IsEnabled = Enable;
-	}
-
-	/// <summary>
-	/// Helper method to get selected policy rule options from the UI checkboxes
-	/// </summary>
-	/// <returns></returns>
-	private List<OptionType> GetSelectedPolicyRuleOptions()
-	{
-		List<OptionType> selectedRules = [];
-
-		// Iterate through each SettingsCard in the PolicyRuleExpander
-		foreach (var item in PolicyRuleExpander.Items)
-		{
-			if (item is SettingsCard settingsCard && settingsCard.Content is CheckBox checkBox && checkBox.IsChecked == true)
-			{
-				// Get the tag of the checkbox
-				string? key = checkBox.Tag?.ToString();
-
-				if (!string.IsNullOrEmpty(key))
-				{
-					selectedRules.Add(CustomDeserialization.ConvertStringToOptionType(key));
-				}
-			}
-		}
-		return selectedRules;
-	}
-
-	/// <summary>
-	/// Uncheck all of the rule options check boxes in the UI
-	/// </summary>
-	private void ClearAllCheckBoxes()
-	{
-		// Iterate through each SettingsCard in the PolicyRuleExpander
-		foreach (var item in PolicyRuleExpander.Items)
-		{
-			if (item is SettingsCard settingsCard && settingsCard.Content is CheckBox checkBox && checkBox.IsChecked == true)
-			{
-				checkBox.IsChecked = false;
-			}
-		}
-	}
-
-	/// <summary>
-	/// Event handler for the flyout's clear button
-	/// </summary>
-	private void PickPolicyFileButton_FlyOut_Clear_Click()
-	{
-		ViewModel.SelectedFilePath = null;
-		ClearAllCheckBoxes();
-	}
-
-	/// <summary>
-	/// Event handlers to retrieve latest policy rule option details from the XML file and check/uncheck UI boxes
-	/// </summary>
-	private async void RefreshRuleOptionsState_Click()
-	{
-		try
-		{
-			ManageButtonStates(false);
-			MainTeachingTip.IsOpen = false;
-
-			if (ViewModel.SelectedFilePath is not null)
-			{
-				await LoadPolicyOptionsFromXML(ViewModel.SelectedFilePath);
-			}
-			else
-			{
-				MainTeachingTip.IsOpen = true;
-				MainTeachingTip.Subtitle = GlobalVars.Rizz.GetString("SelectPolicyFileBeforeRetrievingOptions");
-				return;
-			}
-		}
-		catch (Exception ex)
-		{
-			ViewModel.MainInfoBar.WriteError(ex);
-		}
-		finally
-		{
-			ManageButtonStates(true);
-		}
-	}
 }
--- AppControl Manager/ViewModels/ConfigurePolicyRuleOptionsVM.cs
+++ AppControl Manager/ViewModels/ConfigurePolicyRuleOptionsVM.cs
@@ -15,8 +15,14 @@
 // See here for more information: https://github.com/HotCakeX/Harden-Windows-Security/blob/main/LICENSE
 //
 
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AppControlManager.Main;
 using AppControlManager.Others;
+using AppControlManager.SiPolicy;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
@@ -46,10 +52,351 @@ internal sealed partial class ConfigurePolicyRuleOptionsVM : ViewModelBase
 
 	internal bool IsElevated => App.IsElevated;
 
+	internal bool SettingsExpanderIsExpanded { get; set => SP(ref field, value); }
+
+	internal bool ElementsAreEnabled { get; set => SP(ref field, value); } = true;
+
+	internal bool DeployAfterApplyingToggleButton { get; set => SP(ref field, value); }
+
+	internal string PolicyTemplatesComboBoxSelectedItem { get; set => SP(ref field, value); } = "Base";
+
 	/// <summary>
 	/// To store the selected policy path
 	/// </summary>
 	internal string? SelectedFilePath { get; set => SP(ref field, value); }
+
+	#region CheckBox Properties
+
+	internal bool EnabledUMCICheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledBootMenuProtectionCheckBox { get; set => SP(ref field, value); }
+	internal bool RequiredWHQLCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledAuditModeCheckBox { get; set => SP(ref field, value); }
+	internal bool DisabledFlightSigningCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledInheritDefaultPolicyCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledUnsignedSystemIntegrityPolicyCheckBox { get; set => SP(ref field, value); }
+	internal bool RequiredEVSignersCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledAdvancedBootOptionsMenuCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledBootAuditOnFailureCheckBox { get; set => SP(ref field, value); }
+	internal bool DisabledScriptEnforcementCheckBox { get; set => SP(ref field, value); }
+	internal bool RequiredEnforceStoreApplicationsCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledManagedInstallerCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledIntelligentSecurityGraphAuthorizationCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledInvalidateEAsOnRebootCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledUpdatePolicyNoRebootCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledAllowSupplementalPoliciesCheckBox { get; set => SP(ref field, value); }
+	internal bool DisabledRuntimeFilePathRuleProtectionCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledDynamicCodeSecurityCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledRevokedExpiredAsUnsignedCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledDeveloperModeDynamicCodeTrustCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledSecureSettingPolicyCheckBox { get; set => SP(ref field, value); }
+	internal bool EnabledConditionalWindowsLockdownPolicyCheckBox { get; set => SP(ref field, value); }
+
+	#endregion
+
+	/// <summary>
+	/// Event handler for the browse button
+	/// </summary>
+	internal async void PickPolicyFileButton_Click()
+	{
+		try
+		{
+
+			string? selectedFile = FileDialogHelper.ShowFilePickerDialog(GlobalVars.XMLFilePickerFilter);
+
+			if (!string.IsNullOrWhiteSpace(selectedFile))
+			{
+				SelectedFilePath = selectedFile;
+
+				// Expand the settings expander when user selects a policy
+				SettingsExpanderIsExpanded = true;
+
+				// Load the policy options from the XML and update the UI
+				await LoadPolicyOptionsFromXML(selectedFile);
+			}
+		}
+		catch (Exception ex)
+		{
+			MainInfoBar.WriteError(ex);
+		}
+	}
+
+	/// <summary>
+	/// When the XML policy file is selected by the user, get its rule options and check/uncheck the check boxes in the UI accordingly
+	/// </summary>
+	/// <param name="filePath"></param>
+	internal async Task LoadPolicyOptionsFromXML(string? filePath)
+	{
+		try
+		{
+			SiPolicy.SiPolicy policyObj = null!;
+
+			await Task.Run(() =>
+			{
+				policyObj = Management.Initialize(filePath, null);
+			});
+
+			// All the Policy OptionTypes in the selected XML file
+			List<OptionType> policyRules = policyObj.Rules.Select(x => x.Item).ToList();
+
+			// Update each checkbox state based on the policy rules
+			EnabledUMCICheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:UMCI"));
+			EnabledBootMenuProtectionCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Boot Menu Protection"));
+			RequiredWHQLCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Required:WHQL"));
+			EnabledAuditModeCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Audit Mode"));
+			DisabledFlightSigningCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Disabled:Flight Signing"));
+			EnabledInheritDefaultPolicyCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Inherit Default Policy"));
+			EnabledUnsignedSystemIntegrityPolicyCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Unsigned System Integrity Policy"));
+			RequiredEVSignersCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Required:EV Signers"));
+			EnabledAdvancedBootOptionsMenuCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Advanced Boot Options Menu"));
+			EnabledBootAuditOnFailureCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Boot Audit On Failure"));
+			DisabledScriptEnforcementCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Disabled:Script Enforcement"));
+			RequiredEnforceStoreApplicationsCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Required:Enforce Store Applications"));
+			EnabledManagedInstallerCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Managed Installer"));
+			EnabledIntelligentSecurityGraphAuthorizationCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Intelligent Security Graph Authorization"));
+			EnabledInvalidateEAsOnRebootCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Invalidate EAs on Reboot"));
+			EnabledUpdatePolicyNoRebootCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Update Policy No Reboot"));
+			EnabledAllowSupplementalPoliciesCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Allow Supplemental Policies"));
+			DisabledRuntimeFilePathRuleProtectionCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Disabled:Runtime FilePath Rule Protection"));
+			EnabledDynamicCodeSecurityCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Dynamic Code Security"));
+			EnabledRevokedExpiredAsUnsignedCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Revoked Expired As Unsigned"));
+			EnabledDeveloperModeDynamicCodeTrustCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Developer Mode Dynamic Code Trust"));
+			EnabledSecureSettingPolicyCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Secure Setting Policy"));
+			EnabledConditionalWindowsLockdownPolicyCheckBox = policyRules.Contains(CustomDeserialization.ConvertStringToOptionType("Enabled:Conditional Windows Lockdown Policy"));
+		}
+		catch (Exception ex)
+		{
+			MainInfoBar.WriteError(ex);
+		}
+	}
+
+	/// <summary>
+	/// Event handler for when the Apply button is pressed
+	/// </summary>
+	internal async void ApplyTheChangesButton_Click()
+	{
+
+		try
+		{
+			ElementsAreEnabled = false;
+
+			if (string.IsNullOrWhiteSpace(SelectedFilePath))
+			{
+				MainInfoBar.WriteWarning(GlobalVars.Rizz.GetString("SelectPolicyFileBeforeAddingOptions"));
+				return;
+			}
+
+			// Gather selected rules to add
+			OptionType[] selectedOptions = [.. GetSelectedPolicyRuleOptions()];
+
+			await Task.Run(() =>
+			{
+				CiRuleOptions.Set(SelectedFilePath, rulesToAdd: selectedOptions, RemoveAll: true);
+			});
+
+			if (DeployAfterApplyingToggleButton)
+			{
+				await Task.Run(() =>
+				{
+					DirectoryInfo stagingArea = StagingArea.NewStagingArea("ConfigurePolicyRuleOptionsDeployment");
+
+					string cipPath = Path.Combine(stagingArea.FullName, $"{Path.GetFileName(SelectedFilePath)}.cip");
+
+					SiPolicy.SiPolicy policyObj = Management.Initialize(SelectedFilePath, null);
+
+					if (!policyObj.Rules.Any(x => x.Item is OptionType.EnabledUnsignedSystemIntegrityPolicy))
+					{
+						_ = Dispatcher.TryEnqueue(() =>
+						{
+							MainInfoBar.WriteWarning(GlobalVars.Rizz.GetString("TeachingTipSubtitlePolicyRequiresSigning"));
+						});
+
+						return;
+					}
+
+					Management.ConvertXMLToBinary(SelectedFilePath, null, cipPath);
+
+					// If a base policy is being deployed, ensure it's supplemental policy for AppControl Manager also gets deployed
+					if (SupplementalForSelf.IsEligible(policyObj, SelectedFilePath))
+						SupplementalForSelf.Deploy(stagingArea.FullName, policyObj.PolicyID);
+
+					CiToolHelper.UpdatePolicy(cipPath);
+				});
+			}
+		}
+		catch (Exception ex)
+		{
+			MainInfoBar.WriteError(ex);
+		}
+		finally
+		{
+			ElementsAreEnabled = true;
+		}
+	}
+
+	/// <summary>
+	/// Event handler for the Set button click in the PolicyTemplate section
+	/// </summary>
+	internal async void SetPolicyTemplate_Click()
+	{
+		try
+		{
+			ElementsAreEnabled = false;
+
+			if (string.IsNullOrWhiteSpace(SelectedFilePath))
+			{
+				MainInfoBar.WriteWarning(GlobalVars.Rizz.GetString("SelectPolicyFileBeforeSettingTemplate"));
+				return;
+			}
+
+			// Convert the ComboBoxItem content to the corresponding PolicyTemplate enum value
+			CiRuleOptions.PolicyTemplate template = Enum.Parse<CiRuleOptions.PolicyTemplate>(PolicyTemplatesComboBoxSelectedItem);
+
+			// Call the Set method with only the filePath and template parameters
+			await Task.Run(() =>
+			{
+				CiRuleOptions.Set(SelectedFilePath, template: template);
+			});
+
+			// Refresh the UI check boxes
+			await LoadPolicyOptionsFromXML(SelectedFilePath);
+		}
+		catch (Exception ex)
+		{
+			MainInfoBar.WriteError(ex);
+		}
+		finally
+		{
+			ElementsAreEnabled = true;
+		}
+	}
+
+	/// <summary>
+	/// Event handler for the flyout's clear button
+	/// </summary>
+	internal void PickPolicyFileButton_FlyOut_Clear_Click()
+	{
+		SelectedFilePath = null;
+		ClearAllCheckBoxes();
+	}
+
+	/// <summary>
+	/// Event handlers to retrieve latest policy rule option details from the XML file and check/uncheck UI boxes
+	/// </summary>
+	internal async void RefreshRuleOptionsState_Click()
+	{
+		try
+		{
+			ElementsAreEnabled = false;
+
+			if (SelectedFilePath is not null)
+			{
+				await LoadPolicyOptionsFromXML(SelectedFilePath);
+			}
+			else
+			{
+				MainInfoBar.WriteWarning(GlobalVars.Rizz.GetString("SelectPolicyFileBeforeRetrievingOptions"));
+				return;
+			}
+		}
+		catch (Exception ex)
+		{
+			MainInfoBar.WriteError(ex);
+		}
+		finally
+		{
+			ElementsAreEnabled = true;
+		}
+	}
+
+
+	/// <summary>
+	/// Helper method to get selected policy rule options from the UI checkboxes
+	/// </summary>
+	/// <returns></returns>
+	internal List<OptionType> GetSelectedPolicyRuleOptions()
+	{
+		List<OptionType> selectedRules = [];
+
+		// Check each checkbox individually
+		if (EnabledUMCICheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:UMCI"));
+		if (EnabledBootMenuProtectionCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Boot Menu Protection"));
+		if (RequiredWHQLCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Required:WHQL"));
+		if (EnabledAuditModeCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Audit Mode"));
+		if (DisabledFlightSigningCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Disabled:Flight Signing"));
+		if (EnabledInheritDefaultPolicyCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Inherit Default Policy"));
+		if (EnabledUnsignedSystemIntegrityPolicyCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Unsigned System Integrity Policy"));
+		if (RequiredEVSignersCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Required:EV Signers"));
+		if (EnabledAdvancedBootOptionsMenuCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Advanced Boot Options Menu"));
+		if (EnabledBootAuditOnFailureCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Boot Audit On Failure"));
+		if (DisabledScriptEnforcementCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Disabled:Script Enforcement"));
+		if (RequiredEnforceStoreApplicationsCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Required:Enforce Store Applications"));
+		if (EnabledManagedInstallerCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Managed Installer"));
+		if (EnabledIntelligentSecurityGraphAuthorizationCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Intelligent Security Graph Authorization"));
+		if (EnabledInvalidateEAsOnRebootCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Invalidate EAs on Reboot"));
+		if (EnabledUpdatePolicyNoRebootCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Update Policy No Reboot"));
+		if (EnabledAllowSupplementalPoliciesCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Allow Supplemental Policies"));
+		if (DisabledRuntimeFilePathRuleProtectionCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Disabled:Runtime FilePath Rule Protection"));
+		if (EnabledDynamicCodeSecurityCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Dynamic Code Security"));
+		if (EnabledRevokedExpiredAsUnsignedCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Revoked Expired As Unsigned"));
+		if (EnabledDeveloperModeDynamicCodeTrustCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Developer Mode Dynamic Code Trust"));
+		if (EnabledSecureSettingPolicyCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Secure Setting Policy"));
+		if (EnabledConditionalWindowsLockdownPolicyCheckBox)
+			selectedRules.Add(CustomDeserialization.ConvertStringToOptionType("Enabled:Conditional Windows Lockdown Policy"));
+
+		return selectedRules;
+	}
+
+	/// <summary>
+	/// Uncheck all of the rule options check boxes in the UI
+	/// </summary>
+	internal void ClearAllCheckBoxes()
+	{
+		EnabledUMCICheckBox = false;
+		EnabledBootMenuProtectionCheckBox = false;
+		RequiredWHQLCheckBox = false;
+		EnabledAuditModeCheckBox = false;
+		DisabledFlightSigningCheckBox = false;
+		EnabledInheritDefaultPolicyCheckBox = false;
+		EnabledUnsignedSystemIntegrityPolicyCheckBox = false;
+		RequiredEVSignersCheckBox = false;
+		EnabledAdvancedBootOptionsMenuCheckBox = false;
+		EnabledBootAuditOnFailureCheckBox = false;
+		DisabledScriptEnforcementCheckBox = false;
+		RequiredEnforceStoreApplicationsCheckBox = false;
+		EnabledManagedInstallerCheckBox = false;
+		EnabledIntelligentSecurityGraphAuthorizationCheckBox = false;
+		EnabledInvalidateEAsOnRebootCheckBox = false;
+		EnabledUpdatePolicyNoRebootCheckBox = false;
+		EnabledAllowSupplementalPoliciesCheckBox = false;
+		DisabledRuntimeFilePathRuleProtectionCheckBox = false;
+		EnabledDynamicCodeSecurityCheckBox = false;
+		EnabledRevokedExpiredAsUnsignedCheckBox = false;
+		EnabledDeveloperModeDynamicCodeTrustCheckBox = false;
+		EnabledSecureSettingPolicyCheckBox = false;
+		EnabledConditionalWindowsLockdownPolicyCheckBox = false;
+	}
 
 	internal readonly Dictionary<string, string> RuleOptions = new()
 	{

94396101ace601e7dfe56065f4415371984308a9
offsechq <101721830+OFFSECHQ@users.noreply.github.com>
2026-01-17 11:05:38 +0530

    chore: protect OFFSECHQ customizations from upstream sync

--- Harden-Windows-Security.ps1
+++ Harden-Windows-Security.ps1
@@ -1,9 +1,9 @@
 function P {
-    Write-Warning -Message "The module you're trying to install has been deprecated; Please install the new Harden System Security app from the Microsoft Store: https://apps.microsoft.com/detail/9P7GGFL7DX57"
+    Write-Warning -Message "The module you're trying to install has been deprecated; Please install the new System Security Studio app from: https://github.com/OFFSECHQ/windows-security-studio/releases"
 }
 function AppControl {
     [CmdletBinding()]
     param ([Parameter(Mandatory = $false)][string]$MSIXBundlePath, [Parameter(Mandatory = $False)][string]$SignTool)
-    Write-Warning -Message 'Please install the AppControl Manager from the Microsoft Store: https://apps.microsoft.com/detail/9PNG1JDDTGP8'
-    Write-Warning -Message 'Additionally, both the AppControl Manager and the Harden System Security apps are now capable to install any app package with any certificate.'
+    Write-Warning -Message 'Please install the App Control Studio from: https://github.com/OFFSECHQ/windows-security-studio/releases'
+    Write-Warning -Message 'Both the App Control Studio and the System Security Studio apps are capable of installing any app package with any certificate.'
 }

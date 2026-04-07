374eb5df849cff97fc57bc34aba1a41ddf559a32
Violet Hansen <spynetgirl@outlook.com>
2026-04-07 16:07:40 +0300

    Update Harden-Windows-Security.ps1
    Not necessary anymore, it's been there for many months already.

--- Harden-Windows-Security.ps1
+++ Harden-Windows-Security.ps1
@@ -1,9 +1,3 @@
 function P {
     Write-Warning -Message "The module you're trying to install has been deprecated; Please install the new Harden System Security app from the Microsoft Store: https://apps.microsoft.com/detail/9P7GGFL7DX57"
 }
-function AppControl {
-    [CmdletBinding()]
-    param ([Parameter(Mandatory = $false)][string]$MSIXBundlePath, [Parameter(Mandatory = $False)][string]$SignTool)
-    Write-Warning -Message 'Please install the AppControl Manager from the Microsoft Store: https://apps.microsoft.com/detail/9PNG1JDDTGP8'
-    Write-Warning -Message 'Additionally, both the AppControl Manager and the Harden System Security apps are now capable to install any app package with any certificate.'
-}
